58558862bdb061a4c0f35a2506f3859f3d76f9db
Guy John <guy@rumblesan.com>
2014-07-10 15:29:25 +0100

    Moving more functionality off into EventOps trait

--- src/main/scala/reaktor/EventStream.scala
+++ src/main/scala/reaktor/EventStream.scala
@@ -2,57 +2,13 @@ package com.rumblesan.reaktor
 
 import scalaz._, Scalaz._
 
-class EventStream[InputEvent, OutputEvent](handlerFunc: InputEvent => OutputEvent) {
+class EventStream[InputEvent, OutputEvent](
+  handlerFunc: InputEvent => OutputEvent
+) extends EventOps[InputEvent, OutputEvent] {
 
-  private var listeners: List[OutputEvent => Unit] = Nil
+  override var listeners: List[OutputEvent => Unit] = Nil
 
-  private val handler: InputEvent => OutputEvent = handlerFunc
-
-  type Subscriber[Out] = EventStream[OutputEvent, Out]
-  type Pusher[In] = EventStream[In, OutputEvent]
-
-  def subscribe[SubscriberOutput](
-    subscriber: EventStream[OutputEvent, SubscriberOutput]
-  ): EventStream[OutputEvent, SubscriberOutput] = {
-    val func: OutputEvent => Unit = event => subscriber.push(event)
-    listeners = func :: listeners
-    subscriber
-  }
-
-  def push(event: InputEvent): Unit = {
-
-    val newEvent: OutputEvent = handler(event)
-
-    listeners.map(l => l(newEvent))
-
-    ()
-  }
-
-  def map[SubscriberOutput](func: OutputEvent => SubscriberOutput): Subscriber[SubscriberOutput] = {
-    subscribe(new EventStream[OutputEvent, SubscriberOutput](func))
-  }
-
-  def filter(predicate: OutputEvent => Boolean): Subscriber[OutputEvent] = {
-    val subscriber = EventStream[OutputEvent, OutputEvent](identity)
-
-    val func: OutputEvent => Unit = event => {
-      if (predicate(event)) subscriber.push(event)
-    }
-
-    listeners = func :: listeners
-    subscriber
-  }
-
-  def combine[PusherInput](pusher: Pusher[PusherInput]): EventStream[OutputEvent, OutputEvent] = {
-    val newStream = EventStream[OutputEvent, OutputEvent](identity)
-    subscribe[OutputEvent](newStream)
-    pusher.subscribe[OutputEvent](newStream)
-    newStream
-  }
-
-  def run(func: OutputEvent => Unit): Unit = {
-    listeners = func :: listeners
-  }
+  override val handler: InputEvent => OutputEvent = handlerFunc
 
 }
 
