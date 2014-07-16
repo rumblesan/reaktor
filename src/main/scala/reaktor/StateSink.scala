89b5236affcd5f06b7560df2cf7725d8b78dd6b1
Guy John <guy@rumblesan.com>
2014-07-16 17:34:54 +0100

    Creating StateStream class

--- src/main/scala/reaktor/StateSink.scala
+++ src/main/scala/reaktor/StateSink.scala
@@ -6,28 +6,6 @@ import scalaz.concurrent.{ Actor, Strategy }
 import java.util.concurrent.atomic.AtomicReference
 
 
-class StateSink[InputEvent, StateType, OutputEvent](
-  stateModifier: (StateType, InputEvent) => (StateType, Option[OutputEvent]),
-  initialState: StateType,
-  eventHandler: Option[OutputEvent] => Unit
-)(
-  implicit val strategy: Strategy
-) extends EventStream[InputEvent] {
-
-  private var internalState: AtomicReference[StateType] = new AtomicReference(initialState)
-
-  val actor: Actor[InputEvent] = Actor(event => {
-    val (newState, eventOpt) = stateModifier(internalState.get, event)
-    internalState.set(newState)
-    eventHandler(eventOpt)
-  }, e => println(e.getMessage))
-
-  def apply(event: InputEvent): Unit = actor(event)
-
-  def getState = internalState.get
-
-}
-
 object StateSink {
 
   def apply[InputEvent, StateType](
@@ -35,7 +13,7 @@ object StateSink {
     initialState: StateType
   )(
     implicit strategy: Strategy
-  ) = new StateSink(
+  ) = new StateStream(
     (state: StateType, event: InputEvent) => {
       (stateModifier(state, event), None)
     },
---
+++ src/main/scala/reaktor/StateStream.scala
@@ -0,0 +1,46 @@
+package com.rumblesan.reaktor
+
+import scalaz._, Scalaz._
+import scalaz.concurrent.{ Actor, Strategy }
+
+import java.util.concurrent.atomic.AtomicReference
+
+
+class StateStream[InputEvent, StateType, OutputEvent](
+  stateModifier: (StateType, InputEvent) => (StateType, Option[OutputEvent]),
+  initialState: StateType,
+  eventHandler: OutputEvent => Unit
+)(
+  implicit val strategy: Strategy
+) extends EventStream[InputEvent] {
+
+  private var internalState: AtomicReference[StateType] = new AtomicReference(initialState)
+
+  val actor: Actor[InputEvent] = Actor(event => {
+    val (newState, eventOpt) = stateModifier(internalState.get, event)
+    internalState.set(newState)
+    eventOpt.map(eventHandler)
+  }, e => println(e.getMessage))
+
+  def apply(event: InputEvent): Unit = actor(event)
+
+  def getState = internalState.get
+
+}
+
+object StateStream {
+
+  def apply[InputEvent, StateType, OutputEvent](
+    stateModifier: (StateType, InputEvent) => (StateType, Option[OutputEvent]),
+    initialState: StateType,
+    eventHandler: OutputEvent => Unit
+  )(
+    implicit strategy: Strategy
+  ) = new StateStream(
+    stateModifier,
+    initialState,
+    eventHandler
+  )
+
+}
+
